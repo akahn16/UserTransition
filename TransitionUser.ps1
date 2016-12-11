2def3c50eaef1d0ef72229e4b1f10400c7be4b4f
akahn16 <akahn16@gmail.com>
2016-12-11 11:04:06 -0500

    Added robocopy

--- TransitionUser.ps1
+++ TransitionUser.ps1
@@ -13,6 +13,8 @@ $env:HOMEDRIVE
 $RoamingAppData = ($env:USERPROFILE + '\AppData\Roaming')
 $RoamingAppData
 
+
+
 $isTransitioned = $false
 
 #check to see if AppData is still non-local
@@ -22,21 +24,46 @@ if ($env:APPDATA.Substring(0,2) -ne 'C:') {
 }
 else
 {
+    
+ 
+   If ((Test-Path $OldRedirectedAppData\Transitioned.txt) –eq $True) 
+    {
+    $isTransitioned = $true
+    exit
+    }
+    else
+    {
     write-host 'app data is local'
 
+    
     # ok, apparantly app data is now local, has old redirected AppData been copied to roaming?
 
     if ($isTransitioned -eq $false) {
-        $OldRedirectedAppData = ($env:HOMEDRIVE + '\SomeFolderPath\AppData')
-        $NewRoamingAppData = ($env:USERPROFILE + '\AppData\Roaming')
-        Write-Host ('OldRedirectedAppData: ' + $OldRedirectedAppData)
-        write-host ('NewRoamingAppData: ' + $NewRoamingAppData)
-        # do copy here
+
+        #Acutal Code Begin
+        #$OldRedirectedAppData = ($env:HOMEDRIVE + '\SomeFolderPath\AppData')
+        #$NewRoamingAppData = ($env:USERPROFILE + '\AppData\Roaming')
+        #Write-Host ('OldRedirectedAppData: ' + $OldRedirectedAppData)
+        #write-host ('NewRoamingAppData: ' + $NewRoamingAppData)
+        #Robocopy $OldRedirectedAppData $NewRoamingAppData /mov /mt /e
+        #Out-file $OldRedirectedAppData\Transitioned.txt
+        #Actual Code End
+
+        #Test Code Begin
+        $OldRedirectedAppData='C:\Old'
+        $NewRoamingAppData='C:\New'
+        Robocopy $OldRedirectedAppData $NewRoamingAppData /mov /mt /e
+        Out-file $OldRedirectedAppData\Transitioned.txt
+
+        Start-Sleep -seconds 30
+        logoff
+
     }
 
+
+
+}
 }
 
 
 # check something to determine if transition has occured, if so, exit, otherwise copy stuff
-
-
